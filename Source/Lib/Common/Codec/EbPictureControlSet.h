jerrymiao-tencent/SVT-AV1

Add the ability to perform the global motion search on a down-sampled version of the input

Source/Lib/Common/Codec/EbPictureControlSet.h
@@ -14309,6 +14309,9 @@ extern "C" {
 #if MDC_ADAPTIVE_LEVEL
         uint8_t                                enable_adaptive_ol_partitioning;
 #endif
+#if GM_OPT
+        uint8_t                                use_downsampled_gm_search;
+#endif
     } PictureParentControlSet;
 
     typedef struct PictureControlSetInitData

nekotrix/SVT-AV1-Essential

Source/Lib/Common/Codec/EbPictureControlSet.h
@@ -14309,6 +14309,9 @@ extern "C" {
 #if MDC_ADAPTIVE_LEVEL
         uint8_t                                enable_adaptive_ol_partitioning;
 #endif
+#if GM_OPT
+        uint8_t                                gm_level;
+#endif
     } PictureParentControlSet;
 
     typedef struct PictureControlSetInitData