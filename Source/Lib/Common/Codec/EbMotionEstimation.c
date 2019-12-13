spartazhc/SVT-AV1

16 more tool on/off flags:
edge_skp_angle_intra, inter_intra_compound, fract_search_64, combine_class_12, enable_mfmv, enable_redundant_blk, enable_trellis, spatial_sse_fl, enable_subpel, over_bndry_blk, new_nearest_comb_inject, nx4_4xn_parent_mv_inject, prune_unipred_me, prune_ref_rec_part, nsq_table, frame_end_cdf_update

Source/Lib/Common/Codec/EbMotionEstimation.c
@@ -13676,7 +13676,10 @@ EbErrorType motion_estimate_lcu(
                          picture_control_set_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_2 ||
                          picture_control_set_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_3 )? EB_FALSE : is_nsq_table_used;
 #else
-    is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    if (sequence_control_set_ptr->static_config.nsq_table == AUTO_MODE)
+        is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    else
+        is_nsq_table_used = sequence_control_set_ptr->static_config.nsq_table;
 #endif
 
     if (context_ptr->me_alt_ref == EB_TRUE)

Source/Lib/Common/Codec/EbPictureDecisionProcess.c
@@ -1332,6 +1332,7 @@ EbErrorType signal_derivation_multi_processes_oq(
 #else
         if (picture_control_set_ptr->enc_mode <= ENC_M1 && sequence_control_set_ptr->static_config.encoder_bit_depth == EB_8BIT)
 #endif
+
 #if SPEED_OPT
             picture_control_set_ptr->atb_mode = (MR_MODE || picture_control_set_ptr->temporal_layer_index == 0) ? 1 : 0;
 #else
@@ -1361,12 +1362,16 @@ EbErrorType signal_derivation_multi_processes_oq(
 
         picture_control_set_ptr->wedge_mode = 0;
 
-#if II_COMP_FLAG
         // inter intra pred                      Settings
         // 0                                     OFF
         // 1                                     ON
+        if (sequence_control_set_ptr->static_config.inter_intra_compound == AUTO_MODE)
+#if II_COMP_FLAG
             picture_control_set_ptr->enable_inter_intra = picture_control_set_ptr->slice_type != I_SLICE ? sequence_control_set_ptr->seq_header.enable_interintra_compound : 0;
 #endif
+        else
+            picture_control_set_ptr->enable_inter_intra = sequence_control_set_ptr->static_config.inter_intra_compound;
+
         // Set compound mode      Settings
         // 0                 OFF: No compond mode search : AVG only
         // 1                 ON: compond mode search: AVG/DIST/DIFF
@@ -1376,7 +1381,7 @@ EbErrorType signal_derivation_multi_processes_oq(
             picture_control_set_ptr->enc_mode <= ENC_M1 ? 2 : 1;
         else
             picture_control_set_ptr->compound_mode = 0;
- #if !MULTI_PASS_PD
+#if !MULTI_PASS_PD
         // set compound_types_to_try
         if (picture_control_set_ptr->compound_mode)
             picture_control_set_ptr->compound_types_to_try = picture_control_set_ptr->compound_mode == 1 ? MD_COMP_DIFF0 : MD_COMP_WEDGE;
@@ -1386,18 +1391,26 @@ EbErrorType signal_derivation_multi_processes_oq(
         // Set frame end cdf update mode      Settings
         // 0                                     OFF
         // 1                                     ON
-        if (picture_control_set_ptr->enc_mode == ENC_M0)
-            picture_control_set_ptr->frame_end_cdf_update_mode = 1;
+        if (sequence_control_set_ptr->static_config.frame_end_cdf_update == AUTO_MODE)
+            if (picture_control_set_ptr->enc_mode == ENC_M0)
+                picture_control_set_ptr->frame_end_cdf_update_mode = 1;
+            else
+                picture_control_set_ptr->frame_end_cdf_update_mode = 0;
         else
-            picture_control_set_ptr->frame_end_cdf_update_mode = 0;
+            picture_control_set_ptr->frame_end_cdf_update_mode = sequence_control_set_ptr->static_config.frame_end_cdf_update;
+
+        if (sequence_control_set_ptr->static_config.prune_unipred_me == AUTO_MODE)
 #if M0_OPT
-        if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode >= ENC_M4)
+            if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode >= ENC_M4)
 #else
-        if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 || picture_control_set_ptr->enc_mode >= ENC_M4)
+            if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 || picture_control_set_ptr->enc_mode >= ENC_M4)
 #endif
-            picture_control_set_ptr->prune_unipred_at_me = 0;
+                picture_control_set_ptr->prune_unipred_at_me = 0;
+            else
+                picture_control_set_ptr->prune_unipred_at_me = 1;
         else
-            picture_control_set_ptr->prune_unipred_at_me = 1;
+            picture_control_set_ptr->prune_unipred_at_me = sequence_control_set_ptr->static_config.prune_unipred_me;
+
         //CHKN: Temporal MVP should be disabled for pictures beloning to 4L MiniGop preceeded by 5L miniGOP. in this case the RPS is wrong(known issue). check RPS construction for more info.
         if ((sequence_control_set_ptr->static_config.hierarchical_levels == 4 && picture_control_set_ptr->hierarchical_levels == 3) ||
             picture_control_set_ptr->slice_type == I_SLICE)

Source/Lib/Common/Codec/EbProductCodingLoop.c
@@ -7962,8 +7962,13 @@ void md_encode_block(
         picture_control_set_ptr->parent_pcs_ptr->pic_depth_mode <= PIC_ALL_C_DEPTH_MODE &&
         picture_control_set_ptr->parent_pcs_ptr->nsq_search_level >= NSQ_SEARCH_LEVEL1 &&
         picture_control_set_ptr->parent_pcs_ptr->nsq_search_level < NSQ_SEARCH_FULL) ? EB_TRUE : EB_FALSE;
+
     // To do: MULTI_PASS_PD
-    is_nsq_table_used = picture_control_set_ptr->parent_pcs_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 ? EB_FALSE : is_nsq_table_used;
+    if (sequence_control_set_ptr->static_config.nsq_table == AUTO_MODE)
+        is_nsq_table_used = picture_control_set_ptr->parent_pcs_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 ? EB_FALSE : is_nsq_table_used;
+    else
+        is_nsq_table_used = sequence_control_set_ptr->static_config.nsq_table;
+
 #if ADJUST_NSQ_RANK_BASED_ON_NEIGH
     if (is_nsq_table_used) {
         if (context_ptr->blk_geom->shape == PART_N) {
@@ -8003,7 +8008,10 @@ void md_encode_block(
                          picture_control_set_ptr->parent_pcs_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_2 ||
                          picture_control_set_ptr->parent_pcs_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_3 )? EB_FALSE : is_nsq_table_used;
 #else
-    is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    if (sequence_control_set_ptr->static_config.nsq_table == AUTO_MODE)
+        is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    else
+       is_nsq_table_used = sequence_control_set_ptr->static_config.nsq_table;
 #endif
 
     if (is_nsq_table_used) {

nekotrix/SVT-AV1-Essential

Source/Lib/Common/Codec/EbMotionEstimation.c
@@ -13676,7 +13676,10 @@ EbErrorType motion_estimate_lcu(
                          picture_control_set_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_2 ||
                          picture_control_set_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_3 )? EB_FALSE : is_nsq_table_used;
 #else
-    is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    if (sequence_control_set_ptr->static_config.nsq_table == DEFAULT)
+        is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    else
+        is_nsq_table_used = sequence_control_set_ptr->static_config.nsq_table;
 #endif
 
     if (context_ptr->me_alt_ref == EB_TRUE)

Source/Lib/Common/Codec/EbPictureDecisionProcess.c
@@ -1331,6 +1331,7 @@ EbErrorType signal_derivation_multi_processes_oq(
 #else
         if (picture_control_set_ptr->enc_mode <= ENC_M1 && sequence_control_set_ptr->static_config.encoder_bit_depth == EB_8BIT)
 #endif
+
 #if SPEED_OPT
             picture_control_set_ptr->atb_mode = (MR_MODE || picture_control_set_ptr->temporal_layer_index == 0) ? 1 : 0;
 #else
@@ -1364,12 +1365,16 @@ EbErrorType signal_derivation_multi_processes_oq(
 
         picture_control_set_ptr->wedge_mode = 0;
 
-#if II_COMP_FLAG
         // inter intra pred                      Settings
         // 0                                     OFF
         // 1                                     ON
+#if II_COMP_FLAG
+        if (sequence_control_set_ptr->static_config.inter_intra_compound == DEFAULT)
             picture_control_set_ptr->enable_inter_intra = picture_control_set_ptr->slice_type != I_SLICE ? sequence_control_set_ptr->seq_header.enable_interintra_compound : 0;
+        else
+            picture_control_set_ptr->enable_inter_intra = sequence_control_set_ptr->static_config.inter_intra_compound;
 #endif
+
         // Set compound mode      Settings
         // 0                 OFF: No compond mode search : AVG only
         // 1                 ON: compond mode search: AVG/DIST/DIFF
@@ -1379,7 +1384,7 @@ EbErrorType signal_derivation_multi_processes_oq(
             picture_control_set_ptr->enc_mode <= ENC_M1 ? 2 : 1;
         else
             picture_control_set_ptr->compound_mode = 0;
- #if !MULTI_PASS_PD
+#if !MULTI_PASS_PD
         // set compound_types_to_try
         if (picture_control_set_ptr->compound_mode)
             picture_control_set_ptr->compound_types_to_try = picture_control_set_ptr->compound_mode == 1 ? MD_COMP_DIFF0 : MD_COMP_WEDGE;
@@ -1389,18 +1394,26 @@ EbErrorType signal_derivation_multi_processes_oq(
         // Set frame end cdf update mode      Settings
         // 0                                     OFF
         // 1                                     ON
-        if (picture_control_set_ptr->enc_mode == ENC_M0)
-            picture_control_set_ptr->frame_end_cdf_update_mode = 1;
+        if (sequence_control_set_ptr->static_config.frame_end_cdf_update == DEFAULT)
+            if (picture_control_set_ptr->enc_mode == ENC_M0)
+                picture_control_set_ptr->frame_end_cdf_update_mode = 1;
+            else
+                picture_control_set_ptr->frame_end_cdf_update_mode = 0;
         else
-            picture_control_set_ptr->frame_end_cdf_update_mode = 0;
+            picture_control_set_ptr->frame_end_cdf_update_mode = sequence_control_set_ptr->static_config.frame_end_cdf_update;
+
+        if (sequence_control_set_ptr->static_config.prune_unipred_me == DEFAULT)
 #if M0_OPT
-        if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode >= ENC_M4)
+            if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode >= ENC_M4)
 #else
-        if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 || picture_control_set_ptr->enc_mode >= ENC_M4)
+            if (picture_control_set_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 || picture_control_set_ptr->enc_mode >= ENC_M4)
 #endif
-            picture_control_set_ptr->prune_unipred_at_me = 0;
+                picture_control_set_ptr->prune_unipred_at_me = 0;
+            else
+                picture_control_set_ptr->prune_unipred_at_me = 1;
         else
-            picture_control_set_ptr->prune_unipred_at_me = 1;
+            picture_control_set_ptr->prune_unipred_at_me = sequence_control_set_ptr->static_config.prune_unipred_me;
+
         //CHKN: Temporal MVP should be disabled for pictures beloning to 4L MiniGop preceeded by 5L miniGOP. in this case the RPS is wrong(known issue). check RPS construction for more info.
         if ((sequence_control_set_ptr->static_config.hierarchical_levels == 4 && picture_control_set_ptr->hierarchical_levels == 3) ||
             picture_control_set_ptr->slice_type == I_SLICE)

Source/Lib/Common/Codec/EbProductCodingLoop.c
@@ -8256,8 +8256,13 @@ void md_encode_block(
         picture_control_set_ptr->parent_pcs_ptr->pic_depth_mode <= PIC_ALL_C_DEPTH_MODE &&
         picture_control_set_ptr->parent_pcs_ptr->nsq_search_level >= NSQ_SEARCH_LEVEL1 &&
         picture_control_set_ptr->parent_pcs_ptr->nsq_search_level < NSQ_SEARCH_FULL) ? EB_TRUE : EB_FALSE;
+
     // To do: MULTI_PASS_PD
-    is_nsq_table_used = picture_control_set_ptr->parent_pcs_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 ? EB_FALSE : is_nsq_table_used;
+    if (sequence_control_set_ptr->static_config.nsq_table == DEFAULT)
+        is_nsq_table_used = picture_control_set_ptr->parent_pcs_ptr->sc_content_detected || picture_control_set_ptr->enc_mode == ENC_M0 ? EB_FALSE : is_nsq_table_used;
+    else
+        is_nsq_table_used = sequence_control_set_ptr->static_config.nsq_table;
+
 #if ADJUST_NSQ_RANK_BASED_ON_NEIGH
     if (is_nsq_table_used) {
         if (context_ptr->blk_geom->shape == PART_N) {
@@ -8297,7 +8302,10 @@ void md_encode_block(
                          picture_control_set_ptr->parent_pcs_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_2 ||
                          picture_control_set_ptr->parent_pcs_ptr->pic_depth_mode == PIC_MULTI_PASS_PD_MODE_3 )? EB_FALSE : is_nsq_table_used;
 #else
-    is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    if (sequence_control_set_ptr->static_config.nsq_table == DEFAULT)
+        is_nsq_table_used = picture_control_set_ptr->enc_mode == ENC_M0 ?  EB_FALSE : is_nsq_table_used;
+    else
+        is_nsq_table_used = sequence_control_set_ptr->static_config.nsq_table;
 #endif
 
     if (is_nsq_table_used) {