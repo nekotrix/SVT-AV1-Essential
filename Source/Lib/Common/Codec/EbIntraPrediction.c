spawlows/SVT-AV1

memory optimization, algorithm cleanup , me optimization ,add TPL into look ahead, better speed bdr tread-offs
Co-authored-by: hguermaz <Hassen.Guermazi@intel.com>
Co-authored-by: kelvinhu325 <kelvin.hu@intel.com>
Co-authored-by: Amir Naghdinezhad  <Amir.Naghdinezhad@intel.com>
Co-authored-by: PhoenixWorthVCD  <phoenix.worth@intel.com>
Co-authored-by: lijing0010 <jing.b.li@intel.com>
Co-authored-by: NaderMahdi  <Nader.Mahdi@intel.com>
Co-authored-by: chkngit <Chekib.Nouira@intel.com>

Source/Lib/Common/Codec/EbIntraPrediction.c
@@ -2544,3 +2544,98 @@ void highbd_filter_intra_predictor(uint16_t *dst, ptrdiff_t stride,
         dst += stride;
     }
 }
+
+#if TPL_LA
+static int is_smooth_luma(uint8_t mode) {
+    return (mode == SMOOTH_PRED || mode == SMOOTH_V_PRED || mode == SMOOTH_H_PRED);
+}
+
+void filter_intra_edge(OisMbResults *ois_mb_results_ptr, uint8_t mode, uint16_t max_frame_width, uint16_t max_frame_height,
+                              int32_t p_angle, int32_t cu_origin_x, int32_t cu_origin_y, uint8_t *above_row, uint8_t *left_col) {
+    const int mb_stride = (max_frame_width + 15) / 16;
+    const int mb_height = (max_frame_height + 15) / 16;
+    const int txwpx = tx_size_wide[TX_16X16];
+    const int txhpx = tx_size_high[TX_16X16];
+    const int need_right = p_angle < 90;
+    const int need_bottom = p_angle > 180;
+    int need_left = extend_modes[mode] & NEED_LEFT;
+    int need_above = extend_modes[mode] & NEED_ABOVE;
+    int need_above_left = extend_modes[mode] & NEED_ABOVELEFT;
+    int ab_sm = (cu_origin_y > 0 && (ois_mb_results_ptr - mb_stride)) ? is_smooth_luma((ois_mb_results_ptr - mb_stride)->intra_mode) : 0;
+    int le_sm = (cu_origin_x > 0 && (ois_mb_results_ptr - 1)) ? is_smooth_luma((ois_mb_results_ptr - 1)->intra_mode) : 0;
+    ab_sm = 0; //force to 0 for neighbor may not be ready at segment boundary
+    le_sm = 0; //force to 0 for neighbor may not be ready at segment boundary
+    const int filt_type = (ab_sm || le_sm) ? 1 : 0;
+    int n_top_px  = cu_origin_y > 0 ? AOMMIN(txwpx, (mb_stride * 16 - cu_origin_x + txwpx)) : 0;
+    int n_left_px = cu_origin_x > 0 ? AOMMIN(txhpx, (mb_height * 16 - cu_origin_y + txhpx)) : 0;
+
+    if (av1_is_directional_mode((PredictionMode)mode)) {
+        if (p_angle <= 90)
+            need_above = 1, need_left = 0, need_above_left = 1;
+        else if (p_angle < 180)
+            need_above = 1, need_left = 1, need_above_left = 1;
+        else
+            need_above = 0, need_left = 1, need_above_left = 1;
+    }
+
+    if (p_angle != 90 && p_angle != 180) {
+        const int ab_le = need_above_left ? 1 : 0;
+        if (need_above && need_left && (txwpx + txhpx >= 24)) {
+            filter_intra_edge_corner(above_row, left_col);
+        }
+        if (need_above && n_top_px > 0) {
+            const int strength =
+                intra_edge_filter_strength(txwpx, txhpx, p_angle - 90, filt_type);
+            const int n_px = n_top_px + ab_le + (need_right ? txhpx : 0);
+            eb_av1_filter_intra_edge(above_row - ab_le, n_px, strength);
+        }
+        if (need_left && n_left_px > 0) {
+            const int strength = intra_edge_filter_strength(
+                    txhpx, txwpx, p_angle - 180, filt_type);
+            const int n_px = n_left_px + ab_le + (need_bottom ? txwpx : 0);
+            eb_av1_filter_intra_edge(left_col - ab_le, n_px, strength);
+        }
+    }
+    int upsample_above =
+        use_intra_edge_upsample(txwpx, txhpx, p_angle - 90, filt_type);
+    if (need_above && upsample_above) {
+        const int n_px = txwpx + (need_right ? txhpx : 0);
+        eb_av1_upsample_intra_edge(above_row, n_px);
+    }
+    int upsample_left =
+        use_intra_edge_upsample(txhpx, txwpx, p_angle - 180, filt_type);
+    if (need_left && upsample_left) {
+        const int n_px = txhpx + (need_bottom ? txwpx : 0);
+        eb_av1_upsample_intra_edge(left_col, n_px);
+    }
+    return;
+}
+
+EbErrorType intra_prediction_open_loop_mb(
+     int32_t  p_angle ,
+        uint8_t                          ois_intra_mode,
+        uint32_t                         src_origin_x,
+        uint32_t                         src_origin_y,
+        TxSize                          tx_size,
+        uint8_t                         *above_row,
+        uint8_t                         *left_col,
+        uint8_t                         *dst,
+        uint32_t                        dst_stride)
+
+{
+    EbErrorType                return_error = EB_ErrorNone;
+    PredictionMode mode = ois_intra_mode;
+    const int32_t is_dr_mode = av1_is_directional_mode(mode);
+
+    if (is_dr_mode)
+        dr_predictor(dst, dst_stride, tx_size, above_row, left_col, 0, 0, p_angle);
+    else {
+        // predict
+        if (mode == DC_PRED) {
+            dc_pred[src_origin_x > 0][src_origin_y > 0][tx_size](dst, dst_stride, above_row, left_col);
+        } else
+            pred[mode][tx_size](dst, dst_stride, above_row, left_col);
+    }
+    return return_error;
+}
+#endif

nekotrix/SVT-AV1-Essential

Source/Lib/Common/Codec/EbIntraPrediction.c
@@ -2544,3 +2544,98 @@ void highbd_filter_intra_predictor(uint16_t *dst, ptrdiff_t stride,
         dst += stride;
     }
 }
+
+#if TPL_LA
+static int is_smooth_luma(uint8_t mode) {
+    return (mode == SMOOTH_PRED || mode == SMOOTH_V_PRED || mode == SMOOTH_H_PRED);
+}
+
+void filter_intra_edge(OisMbResults *ois_mb_results_ptr, uint8_t mode, uint16_t max_frame_width, uint16_t max_frame_height,
+                              int32_t p_angle, int32_t cu_origin_x, int32_t cu_origin_y, uint8_t *above_row, uint8_t *left_col) {
+    const int mb_stride = (max_frame_width + 15) / 16;
+    const int mb_height = (max_frame_height + 15) / 16;
+    const int txwpx = tx_size_wide[TX_16X16];
+    const int txhpx = tx_size_high[TX_16X16];
+    const int need_right = p_angle < 90;
+    const int need_bottom = p_angle > 180;
+    int need_left = extend_modes[mode] & NEED_LEFT;
+    int need_above = extend_modes[mode] & NEED_ABOVE;
+    int need_above_left = extend_modes[mode] & NEED_ABOVELEFT;
+    int ab_sm = (cu_origin_y > 0 && (ois_mb_results_ptr - mb_stride)) ? is_smooth_luma((ois_mb_results_ptr - mb_stride)->intra_mode) : 0;
+    int le_sm = (cu_origin_x > 0 && (ois_mb_results_ptr - 1)) ? is_smooth_luma((ois_mb_results_ptr - 1)->intra_mode) : 0;
+    ab_sm = 0; //force to 0 for neighbor may not be ready at segment boundary
+    le_sm = 0; //force to 0 for neighbor may not be ready at segment boundary
+    const int filt_type = (ab_sm || le_sm) ? 1 : 0;
+    int n_top_px  = cu_origin_y > 0 ? AOMMIN(txwpx, (mb_stride * 16 - cu_origin_x + txwpx)) : 0;
+    int n_left_px = cu_origin_x > 0 ? AOMMIN(txhpx, (mb_height * 16 - cu_origin_y + txhpx)) : 0;
+
+    if (av1_is_directional_mode((PredictionMode)mode)) {
+        if (p_angle <= 90)
+            need_above = 1, need_left = 0, need_above_left = 1;
+        else if (p_angle < 180)
+            need_above = 1, need_left = 1, need_above_left = 1;
+        else
+            need_above = 0, need_left = 1, need_above_left = 1;
+    }
+
+    if (p_angle != 90 && p_angle != 180) {
+        const int ab_le = need_above_left ? 1 : 0;
+        if (need_above && need_left && (txwpx + txhpx >= 24)) {
+            filter_intra_edge_corner(above_row, left_col);
+        }
+        if (need_above && n_top_px > 0) {
+            const int strength =
+                intra_edge_filter_strength(txwpx, txhpx, p_angle - 90, filt_type);
+            const int n_px = n_top_px + ab_le + (need_right ? txhpx : 0);
+            eb_av1_filter_intra_edge(above_row - ab_le, n_px, strength);
+        }
+        if (need_left && n_left_px > 0) {
+            const int strength = intra_edge_filter_strength(
+                    txhpx, txwpx, p_angle - 180, filt_type);
+            const int n_px = n_left_px + ab_le + (need_bottom ? txwpx : 0);
+            eb_av1_filter_intra_edge(left_col - ab_le, n_px, strength);
+        }
+    }
+    int upsample_above =
+        use_intra_edge_upsample(txwpx, txhpx, p_angle - 90, filt_type);
+    if (need_above && upsample_above) {
+        const int n_px = txwpx + (need_right ? txhpx : 0);
+        eb_av1_upsample_intra_edge(above_row, n_px);
+    }
+    int upsample_left =
+        use_intra_edge_upsample(txhpx, txwpx, p_angle - 180, filt_type);
+    if (need_left && upsample_left) {
+        const int n_px = txhpx + (need_bottom ? txwpx : 0);
+        eb_av1_upsample_intra_edge(left_col, n_px);
+    }
+    return;
+}
+
+EbErrorType intra_prediction_open_loop_mb(
+     int32_t  p_angle ,
+        uint8_t                          ois_intra_mode,
+        uint32_t                         src_origin_x,
+        uint32_t                         src_origin_y,
+        TxSize                          tx_size,
+        uint8_t                         *above_row,
+        uint8_t                         *left_col,
+        uint8_t                         *dst,
+        uint32_t                        dst_stride)
+
+{
+    EbErrorType                return_error = EB_ErrorNone;
+    PredictionMode mode = ois_intra_mode;
+    const int32_t is_dr_mode = av1_is_directional_mode(mode);
+
+    if (is_dr_mode)
+        dr_predictor(dst, dst_stride, tx_size, above_row, left_col, 0, 0, p_angle);
+    else {
+        // predict
+        if (mode == DC_PRED) {
+            dc_pred[src_origin_x > 0][src_origin_y > 0][tx_size](dst, dst_stride, above_row, left_col);
+        } else
+            eb_pred[mode][tx_size](dst, dst_stride, above_row, left_col);
+    }
+    return return_error;
+}
+#endif