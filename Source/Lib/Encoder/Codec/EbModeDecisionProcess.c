gianni-rosato/svt-av1-psy

add support to allow users to pass into scale factors for lambda values for different frame types

Source/Lib/Encoder/Codec/EbModeDecisionProcess.c
@@ -536,6 +536,15 @@ static void av1_lambda_assign_md(PictureControlSet *pcs, ModeDecisionContext *ct
 
     ctx->full_lambda_md[1] *= 16;
     ctx->fast_lambda_md[1] *= 4;
+
+    SequenceControlSet *scs_ptr = pcs->scs_ptr;
+    uint32_t            scale_factor =
+        scs_ptr->static_config.lambda_scale_factors[pcs->parent_pcs_ptr->update_type];
+    ctx->full_lambda_md[0] = (ctx->full_lambda_md[0] * scale_factor) >> 7;
+    ctx->full_lambda_md[1] = (ctx->full_lambda_md[1] * scale_factor) >> 7;
+    ctx->fast_lambda_md[0] = (ctx->fast_lambda_md[0] * scale_factor) >> 7;
+    ctx->fast_lambda_md[1] = (ctx->fast_lambda_md[1] * scale_factor) >> 7;
+
     ctx->full_sb_lambda_md[0] = ctx->full_lambda_md[0];
     ctx->full_sb_lambda_md[1] = ctx->full_lambda_md[1];
 }
@@ -564,7 +573,12 @@ void av1_lambda_assign(PictureControlSet *pcs_ptr, uint32_t *fast_lambda, uint32
     }
 
     // NM: To be done: tune lambda based on the picture type and layer.
+    SequenceControlSet *scs_ptr = pcs_ptr->scs_ptr;
+    uint32_t scale_factor = scs_ptr->static_config.lambda_scale_factors[pcs_ptr->parent_pcs_ptr->update_type];
+    *full_lambda                     = (*full_lambda * scale_factor) >> 7;
+    *fast_lambda                     = (*fast_lambda * scale_factor) >> 7;
 }
+
 const EbAv1LambdaAssignFunc av1_lambda_assignment_function_table[4] = {
     av1_lambda_assign,
     av1_lambda_assign,

Source/Lib/Encoder/Globals/EbEncHandle.c
@@ -3954,6 +3954,9 @@ void copy_api_from_app(
     scs_ptr->static_config.chroma_v_ac_qindex_offset =
       MAX(-64, MIN(((EbSvtAv1EncConfiguration*)config_struct)->chroma_v_ac_qindex_offset, 63));
 
+    memcpy(scs_ptr->static_config.lambda_scale_factors, config_struct->lambda_scale_factors,
+        FRAME_UPDATE_TYPES * sizeof(uint32_t));
+
     scs_ptr->static_config.rc_stats_buffer = ((EbSvtAv1EncConfiguration*)config_struct)->rc_stats_buffer;
     scs_ptr->static_config.pass = ((EbSvtAv1EncConfiguration*)config_struct)->pass;
     // Deblock Filter

nekotrix/SVT-AV1-Essential

Source/Lib/Encoder/Codec/EbModeDecisionProcess.c
@@ -536,6 +536,15 @@ static void av1_lambda_assign_md(PictureControlSet *pcs, ModeDecisionContext *ct
 
     ctx->full_lambda_md[1] *= 16;
     ctx->fast_lambda_md[1] *= 4;
+
+    SequenceControlSet *scs_ptr = pcs->scs_ptr;
+    uint32_t            scale_factor =
+        scs_ptr->static_config.lambda_scale_factors[pcs->parent_pcs_ptr->update_type];
+    ctx->full_lambda_md[0] = (ctx->full_lambda_md[0] * scale_factor) >> 7;
+    ctx->full_lambda_md[1] = (ctx->full_lambda_md[1] * scale_factor) >> 7;
+    ctx->fast_lambda_md[0] = (ctx->fast_lambda_md[0] * scale_factor) >> 7;
+    ctx->fast_lambda_md[1] = (ctx->fast_lambda_md[1] * scale_factor) >> 7;
+
     ctx->full_sb_lambda_md[0] = ctx->full_lambda_md[0];
     ctx->full_sb_lambda_md[1] = ctx->full_lambda_md[1];
 }
@@ -564,7 +573,12 @@ void av1_lambda_assign(PictureControlSet *pcs_ptr, uint32_t *fast_lambda, uint32
     }
 
     // NM: To be done: tune lambda based on the picture type and layer.
+    SequenceControlSet *scs = pcs_ptr->scs_ptr;
+    uint32_t scale_factor = scs->static_config.lambda_scale_factors[pcs_ptr->parent_pcs_ptr->update_type];
+    *full_lambda                     = (*full_lambda * scale_factor) >> 7;
+    *fast_lambda                     = (*fast_lambda * scale_factor) >> 7;
 }
+
 const EbAv1LambdaAssignFunc av1_lambda_assignment_function_table[4] = {
     av1_lambda_assign,
     av1_lambda_assign,

Source/Lib/Encoder/Globals/EbEncHandle.c
@@ -3954,6 +3954,9 @@ void copy_api_from_app(
     scs_ptr->static_config.chroma_v_ac_qindex_offset =
       MAX(-64, MIN(((EbSvtAv1EncConfiguration*)config_struct)->chroma_v_ac_qindex_offset, 63));
 
+    memcpy(scs_ptr->static_config.lambda_scale_factors, config_struct->lambda_scale_factors,
+        FRAME_UPDATE_TYPES * sizeof(int32_t));
+
     scs_ptr->static_config.rc_stats_buffer = ((EbSvtAv1EncConfiguration*)config_struct)->rc_stats_buffer;
     scs_ptr->static_config.pass = ((EbSvtAv1EncConfiguration*)config_struct)->pass;
     // Deblock Filter