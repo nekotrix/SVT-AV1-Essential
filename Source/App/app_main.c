a203f62d375d3aa6cdabffe60f33442ae33ff81e
nekotrix <93526043+nekotrix@users.noreply.github.com>
2026-03-14 15:01:52 +0100

    Introduce full-help

--- Source/App/app_main.c
+++ Source/App/app_main.c
@@ -422,6 +422,9 @@ int main(int argc, char* argv[]) {
     if (get_help(argc, argv))
         return 0;
 
+    if (get_fhelp(argc, argv))
+        return 0;
+
     if (get_color_help(argc, argv))
         return 0;
 
